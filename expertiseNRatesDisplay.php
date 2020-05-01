d65508ce47418d0eb264c65039e8b75e20a78b89
Harsh <gurjarhariom3114@gmil.com>
2020-05-01 17:51:41 +0530

    comment added

--- expertiseNRatesDisplay.php
+++ expertiseNRatesDisplay.php
@@ -10,18 +10,18 @@
 
 <?php
 
-$connection = mysqli_connect("localhost", "root", "","skilldb");
+$connection = mysqli_connect("localhost", "root", "","skilldb");   //making connection to to the server
 
-$sql= "SELECT * FROM skills";
+$sql= "SELECT * FROM skills";                                      //fetching the data from database
 $result = mysqli_query($connection, $sql);
-$json_array = array();
+$json_array = array();                                             //making an array to store data
 
 while( $row = mysqli_fetch_assoc( $result ) ){
     $json_array[]=$row;
 
 }
 
-echo json_encode($json_array);
+echo json_encode($json_array);  //changing the json data into araay string.
 // echo '<pre>';
 // print_r($json_array);
 // echo '</pre>';
